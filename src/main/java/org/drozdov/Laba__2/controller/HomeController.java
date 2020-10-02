d46d3d821ef15eddadaa6eff2363b17a54b3514d
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-10-02 22:05:46 +0300

    Commit 7

--- src/main/java/org/drozdov/Laba__2/controller/HomeController.java
+++ src/main/java/org/drozdov/Laba__2/controller/HomeController.java
@@ -4,21 +4,22 @@ import org.drozdov.Laba__2.implement.JavaConfig;
 
 import org.drozdov.Laba__2.inetrface.Sender;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-@RestController
+@Controller
 public class HomeController {
 
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
 
     @RequestMapping(value = "/")
-    public String home(){
+    public String home(Model model){
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean("wirelessSenderImpl",Sender.class);
-        return sender.sendMessage("Message from Java config");
+        model.addAttribute("msg",sender.sendMessage("Message from Java config"));
+        return "test";
     }
 }
 
