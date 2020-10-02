f1f29f2b7a5746e2c857a23f9601b2033dadead4
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-10-02 20:06:57 +0300

    Commit 4

--- src/main/java/org/drozdov/Laba__2/controller/HomeController.java
+++ src/main/java/org/drozdov/Laba__2/controller/HomeController.java
@@ -5,21 +5,20 @@ import org.drozdov.Laba__2.implement.SenderImpl;
 import org.drozdov.Laba__2.implement.WirelessSenderImpl;
 import org.drozdov.Laba__2.inetrface.Receiver;
 import org.drozdov.Laba__2.inetrface.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    private final Sender sender;
-    private final Receiver receiver;
+    @Autowired
+    private org.springframework.web.context.WebApplicationContext context;
 
-    public HomeController() {
-        this.receiver = new ReceiverImpl();
-        this.sender = new WirelessSenderImpl(receiver);
-    }
+
     @RequestMapping(value = "/")
     public String home(){
-        return sender.sendMessage("Hello world");
+        Sender sender = context.getBean("sender",Sender.class);
+        return sender.sendMessage("My Message");
     }
 }
 
