0d8f5b15419ae35cdb6bed72686f4addf2dc2c38
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-10-02 22:30:29 +0300

    Commit 9

--- src/main/java/org/drozdov/Laba__2/controller/HomeController.java
+++ src/main/java/org/drozdov/Laba__2/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.drozdov.Laba__2.inetrface.Sender;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,8 +16,8 @@ public class HomeController {
     private org.springframework.context.annotation.AnnotationConfigApplicationContext context;
 
 
-    @RequestMapping(value = "/")
-    public String home(@RequestParam(value = "name", required = false, defaultValue = "stranger") String name, Model model){
+    @RequestMapping(value = "/{name}")
+    public String home(@PathVariable("name") String name, Model model){
         context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Sender sender = context.getBean("wirelessSenderImpl",Sender.class);
         model.addAttribute("msg",sender.sendMessage("Hello "+name+"!"));
