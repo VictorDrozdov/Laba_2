fb2eff775af7633c7211ec8e3709a02b50367497
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-10-06 18:21:30 +0300

    Commit 1

--- src/main/java/org/drozdov/Laba__2/controller/HomeController.java
+++ src/main/java/org/drozdov/Laba__2/controller/HomeController.java
@@ -1,14 +1,19 @@
 package org.drozdov.Laba__2.controller;
+//import com.sun.tools.javac.util.List;
+//import org.drozdov.Laba__2.*;
+import  java.util.List;
 
+import org.drozdov.Laba__2.entity.User;
 import org.drozdov.Laba__2.implement.JavaConfig;
-
 import org.drozdov.Laba__2.inetrface.Sender;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+
 
 @Controller
 public class HomeController {
@@ -22,6 +27,16 @@ public class HomeController {
         Sender sender = context.getBean("wirelessSenderImpl",Sender.class);
         model.addAttribute("msg",sender.sendMessage("Hello "+name+"!"));
         return "test";
+    }
+    @RequestMapping(value = "/")
+    public String getUser(Model model){
+        Collection<User> users = List.of(
+                new User("jon", "smit", "fjk@jkf.ru"),
+                new User("mikle", "jonson", "dk@fj.ru")
+        );
+        model.addAttribute("users", users);
+        return "users";
+
     }
 }
 
