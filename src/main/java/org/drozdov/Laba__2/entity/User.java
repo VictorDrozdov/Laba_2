e501228812552daebb3180bcb51eec4f8d8d629f
Victor Drozdov <victoraleksandrovich4@gmail.com>
2020-10-19 11:31:58 +0300

    Laba3_5

---
+++ src/main/java/org/drozdov/Laba__2/entity/User.java
@@ -0,0 +1,41 @@
+package org.drozdov.Laba__2.entity;
+
+public class User {
+    private String name;
+    private String surname;
+    private  String email;
+
+    public User(){
+        
+    }
+
+    public User(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
